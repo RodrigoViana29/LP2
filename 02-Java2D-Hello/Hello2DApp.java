ed846f522ab33dd5833803de0f02029e2e9e931b
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2021-08-09 17:07:28 -0300

    Add files via upload

---
+++ 02-Java2D-Hello/Hello2DApp.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Hello2DApp {
+    public static void main (String[] args) {
+        Hello2DFrame frame = new Hello2DFrame();
+    }
+}
+
+class Hello2DFrame extends JFrame {
+    public Hello2DFrame () {
+        this.addWindowListener (
+            new WindowAdapter() {
+                public void windowClosing (WindowEvent e) {
+                    System.exit(0);
+                }
+            }
+        );
+        this.setTitle("Java2D - Hello World!");
+        this.setSize(350, 350);
+        this.setVisible(true);
+	this.getContentPane().setBackground(Color.blue);
+    }
+
+    public void paint (Graphics g) {
+        super.paint(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(Color.red);
+        int w = getWidth();
+        int h = getHeight();
+        g2d.drawLine(0,0, w,h);
+        g2d.drawLine(0,h, w,0);
+	g2d.drawOval(0,0, w,h);
+        g2d.drawOval(0,h, w,0);
+	g2d.drawLine(175,0, 175,h);
+    }
+}
