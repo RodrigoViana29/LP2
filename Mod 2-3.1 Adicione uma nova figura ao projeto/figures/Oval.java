342dd2446c70b9b6189cefe29802909aeb03b1db
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2021-08-23 20:00:32 -0300

    Add files via upload

---
+++ Mod 2-3.1 Adicione uma nova figura ao projeto/figures/Oval.java
@@ -0,0 +1,25 @@
+package figures;
+
+import java.awt.*;
+
+public class Oval {
+    private int x, y;
+    private int w, h;
+
+    public Oval (int x, int y, int w, int h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    public void print () {
+        System.out.format("Circulo de tamanho (%d,%d) na posicao (%d,%d).\n",
+            this.w, this.h, this.x, this.y);
+    }
+
+    public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawOval(this.x,this.y, this.w,this.h);
+    }
+}
