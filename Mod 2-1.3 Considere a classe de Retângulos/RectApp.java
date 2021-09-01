201d7a285301d8e567038570a952dbbdc4397510
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2021-09-01 18:58:05 -0300

    Update RectApp.java

--- Mod 2-1.3 Considere a classe de Retângulos/RectApp.java
+++ Mod 2-1.3 Considere a classe de Retângulos/RectApp.java
@@ -1,7 +1,6 @@
 public class RectApp {
     public static void main (String[] args) {
         Rect r1 = new Rect(4,4, 30,20);
-	r1.print();
 	r1.area();
 	r1.drag(2,2);
 	
@@ -16,19 +15,15 @@ class Rect {
         this.w = w;
         this.h = h;
     }
-    void print () {
-        System.out.format("Retangulo de tamanho (%d,%d) na posicao (%d,%d).\n",
-            this.w, this.h, this.x, this.y);
-    }
     int area (){
 	 int area = this.w * this.h;
-	 System.out.format("A area do retangulo é : (%d).\n",area);
+	 System.out.format("A area do retangulo Ã© : (%d).\n",area);
 	 return area;
      }	
      void drag(int dx,int dy){
 	this.x = this.x + dx;
 	this.y = this.y + dy;
-	System.out.format("A nova posicao é : (%d,%d).\n",this.x,this.y);
+	System.out.format("A nova posicao Ã© : (%d,%d).\n",this.x,this.y);
      }
 	
      
