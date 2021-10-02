8ae949173110a5d3373719adb8f5762b21cca203
RodrigoViana29 <88325159+RodrigoViana29@users.noreply.github.com>
2021-10-01 21:31:01 -0300

    Delete IVisible.java

--- interfaces/IVisible.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.awt.*;
-
-public interface IVisible {
-    public boolean IsInsideFigure(Point mousePointPosition);
-    public void Paint(Graphics g);
-    public void applyRedSelection(Graphics g);
-}
